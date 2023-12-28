c971d4cff0e8b88f26e1e4222c3eeda776bfafc2
Jo Zou <jo_zou@wistron.com>
2023-12-28 16:56:38 +0800

    增加服务resource

--- jpa-rest/src/main/java/com/example/jparest/controller/FileUpload.java
+++ jpa-rest/src/main/java/com/example/jparest/controller/FileUpload.java
@@ -1,10 +1,14 @@
 package com.example.jparest.controller;
 
 import cn.hutool.core.io.resource.ResourceUtil;
-//import com.alibaba.excel.EasyExcel;
+import com.example.jparest.service.ExcelTemplateFillService;
 import com.example.jparest.utils.FileUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +20,9 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 public class FileUpload {
+
+    @Autowired
+    ExcelTemplateFillService excelTemplateFillService;
 
     @PostMapping("file")
     public String saveFile(@RequestParam("file") MultipartFile file) throws IOException {
@@ -33,5 +40,20 @@ public class FileUpload {
         String s = ResourceUtil.readUtf8Str("category.json");
         System.out.println(s);
 //        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+    }
+
+    @PostMapping(value = "/fillTemplate")
+    public ResponseEntity<ByteArrayResource> fillTemplate(MultipartFile file, String vars, String data) throws IOException {
+        return excelTemplateFillService.fillTemplate(file,vars,data);
+    }
+
+    @PostMapping(value = "/fillExcel")
+    public void fillTemplate(MultipartFile file, String vars, String data, HttpServletResponse response) throws IOException {
+        excelTemplateFillService.fillTemplate(file,vars,data,response);
+    }
+
+    @GetMapping(value = "/fill")
+    public void fillTemplate(HttpServletResponse response) throws IOException {
+        excelTemplateFillService.fillTemplate(response);
     }
 }
