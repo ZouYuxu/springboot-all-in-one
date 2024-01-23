6f3286e269915e5fc2fcea05b545a1acbe54c8c5
Jo Zou <jo_zou@wistron.com>
2024-01-23 08:38:08 +0800

    my exception util

--- jpa-rest/src/main/java/com/example/jparest/utils/MyExceptionUtil.java
+++ jpa-rest/src/main/java/com/example/jparest/utils/MyExceptionUtil.java
@@ -1,6 +1,6 @@
 package com.example.jparest.utils;
 
-public class MyExceptionUtil extends Throwable {
+public class MyExceptionUtil  extends RuntimeException {
     public MyExceptionUtil(String s, Exception e) {
         super(s, e);
     }
